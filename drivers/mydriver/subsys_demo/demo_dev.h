d3f7a84920678438c364d33f260ed1405cc39c76
hominlinx <monkeyyuan635@gmail.com>
2016-11-05 19:51:33 -0700

    add subsys demo

---
+++ drivers/mydriver/subsys_demo/demo_dev.h
@@ -0,0 +1,14 @@
+/*
+ * Generic demo dev interface.
+ *
+*/
+
+#ifndef _DEMO_DEV_H_
+#define _DEMO_DEV_H_
+
+
+#define DEMO_IOCTL_SET  _IOW('p', 0x01, struct demo_ctl_data) /* Set demo data  */
+#define DEMO_IOCTL_GET  _IOR('p', 0x02, struct demo_ctl_data) /* Read demo data */
+
+
+#endif /* _DEMO_DEV_H_ */
